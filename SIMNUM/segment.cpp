d8fa6b82e5aef82f71e75b9ca6870e4df4fee0f0
maman2015 <chehade@ensta.fr>
2014-02-10 12:09:53 +0100

    segment

--- SIMNUM/segment.cpp
+++ SIMNUM/segment.cpp
@@ -1,5 +1,54 @@
 #include "segment.h"
+#include "math.h"
 
-segment::segment()
+
+segment::segment() //constructeur par défaut
 {
+    sommet S1;
+    sommet S2;
 }
+
+segment::segment(sommet A, sommet B) //constructeur avec valeur
+{
+    S1=A;
+    S2=B;
+}
+
+pairsom segment::getSommet()
+  {
+    pairsom pair=std::make_pair(S1, S2);
+    return pair;
+}
+
+segment::~segment() //destructeur
+{ delete [] &S1;
+     delete [] &S2;
+}
+
+segment & segment::operator = (segment & seg) //operateur =
+{
+    //delete &S1;
+    //delete &S2;
+
+ pairsom pair=std::make_pair(S1, S2);
+ pair.first;
+
+    S1 =(seg.getSommet()).first;
+    S2 = seg.getSommet().second;
+    return * this;
+};
+float  longueur(const segment & seg)
+{
+    float l
+    S1= seg.getSommet().first;
+    S2= seg.getSommet().second;
+    x1= S1.Xcoord();
+    y1= S1.Ycoord();
+    x2= S2.Xcoord();
+    y2= S2.Ycoord();
+    l = sqrt((x2-x1)^2+(y2-y1)^2);;
+    return l;
+
+}
+
+
--- SIMNUM/segment.h
+++ SIMNUM/segment.h
@@ -1,10 +1,28 @@
 #ifndef SEGMENT_H
 #define SEGMENT_H
+#include <stdlib.h>
+#include "utility"
+#include "sommet.h"
+
+
+
+class sommet;
+typedef std::pair<sommet, sommet> pairsom;
 
 class segment
 {
+private :
+    sommet S1;
+    sommet S2;
 public:
+
+    pairsom getSommet();
     segment();
+    segment(sommet A, sommet B);
+    ~segment ();
+    segment & operator = (segment & seg);
+    float  longueur(const segment & seg);
+
 };
 
 #endif // SEGMENT_H
