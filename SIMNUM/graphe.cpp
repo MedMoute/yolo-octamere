227a30034be979e702d0a52aae6d50e4064c71e6
maman2015 <chehade@ensta.fr>
2014-02-15 13:51:18 +0100

    graphe

--- SIMNUM/graphe.cpp
+++ SIMNUM/graphe.cpp
@@ -1,5 +1,28 @@
 #include "graphe.h"
 
-graphe::graphe()
+graphe::graphe() //constructeur par défaut
 {
+
+}
+
+std::list<arc> graphe::getGraph() //
+{
+    return arcs;
+}
+
+graphe::graphe(graphe & grph) //constructeur par copie
+{
+  arcs=grph.getGraph();
+
+}
+
+graphe::~graphe() //destructor
+{
+    delete [] &arcs;
+}
+
+graphe & graphe::operator = (const graphe & grph) //opérateur =
+{
+    arcs=grph.getGraph();
+    return * this;
 }
